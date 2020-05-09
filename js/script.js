a8edd768d823f20110d1f365e3a802ca8d7f645e
squarlll <lilsquarl@gmail.com>
2020-05-09 13:27:10 +0300

    авыф

--- js/script.js
+++ js/script.js
@@ -1,7 +1,72 @@
 $(document).ready(function(){
-	
+	baguetteBox.run('#lightgallery');
+	$('.menu__list').on('click', function(){
+		$(this).toggleClass('active');
+		$(this).siblings().removeClass('active');
+	});
+
+	$('#search').on('click', function(){
+		$('#search').fadeOut(0); 
+		$('#search-exit').fadeIn(0);
+		$('#input-search').fadeIn(300);
+		$('#search-input').fadeIn(300);
+	});
+	$('#search-exit').on('click', function(){
+		$('#search-exit').fadeOut(0);
+		$('#input-search').fadeOut(300);
+		$('#search-input').fadeOut(300);
+		$('#search').fadeIn(0);
+	});
+	$('.menu-1').on('click', function(){
+		$('.menu-1').toggleClass('menu-1_active');
+		$('.header__content-wrapper').toggleClass('header__content-wrapper_active');
+	});
+	$('.menu-2').on('click', function(){
+		$('.menu-2').toggleClass('menu-2_active');
+		$('.header__news').toggleClass('active');
+		$('.menu-1').removeClass('menu-1_active');
+		$('.header__content-wrapper').removeClass('header__content-wrapper_active');
+	});
+	$('.dragons').on('click', function(){
+		$('.menu-1').removeClass('menu-1_active');
+		$('.header__content-wrapper').removeClass('header__content-wrapper_active');
+		$('.header__news').slideUp(300);
+		$('.menu-2').removeClass('menu-2_active');
+	});
 });
-// slider
+$(document).on("scroll", function(){
+    if($(document).scrollTop()>120){
+        $("header").addClass("header_small");
+    } else{
+        $("header").removeClass("header_small");
+    }
+});
+$('.main').slick({
+	infinite: true,
+	arrows: true,
+	slidesToShow:1,
+	speed:800,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				dots: true
+			}
+		}
+	]
+});
+$('.header__slider').slick({
+	infinite: true,
+	arrows: true,
+	slidesToShow:1,
+	speed:300,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	appendArrows: '.header__slider-arrows',
+	prevArrow: '<button class="slick-arrow slick-prev"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 492 492" style="enable-background:new 0 0 492 492;" xml:space="preserve"><g><g><path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12    C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084    c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864    l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button>',
+	nextArrow: '<button class="slick-arrow slick-next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve"><g><g><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button>'
+});
 $('.dragons__slider').slick({
 	infinite: true,
 	arrows: true,
@@ -10,4 +75,32 @@ $('.dragons__slider').slick({
 	appendArrows: '.slider-arrows',
 	prevArrow: '<button class="slick-arrow slick-prev"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 492 492" style="enable-background:new 0 0 492 492;" xml:space="preserve"><g><g><path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12    C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084    c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864    l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button>',
 	nextArrow: '<button class="slick-arrow slick-next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 492.004 492.004;" xml:space="preserve"><g><g><path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button>'
+});
+$('.slider_1').slick({
+	infinite: true,
+	arrows: true,
+	slidesToShow:4,
+	speed:250,
+	autoplay: true,
+	autoplaySpeed: 4000,
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+	]
 });
